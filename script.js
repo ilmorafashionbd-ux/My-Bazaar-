8162106965c2eefc52573729c0852ee93b3ac08a
ilmorafashionbd-ux <ilmorafashionbd@gmail.com>
2025-08-21 23:51:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const DATA_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRPm9-h3hnXGp1r7HBXl6qam4_s8v1SNKnp0Xwa-VdrxJXRRaQihnxKl51fIGuLF6I4VLhGRZ0cHAv9/pub?gid=0&single=true&output=csv";
 
 // GitHub Image Base Path
-const imageBaseURL = "https://ilmorafashionbd-ux.github.io/My-Bazaar-/images/";
+const imageBaseURL = "https://ilmorafashionbd-ux.github.io/My-Bazaar-/images/product%202";
 
 // WhatsApp Number
 const WHATSAPP = "8801778095805";
